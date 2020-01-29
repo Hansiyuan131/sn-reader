63b4ac5dd08e418f120dcce4f5f084403aecb105
Hansiyuan131 <422295068@qq.com>
2020-01-29 22:07:36 +0800

    init project

---
+++ sn-reader-server/src/main/java/com/snreader/snreaderserver/SnReaderServerApplication.java
@@ -0,0 +1,13 @@
+package com.snreader.snreaderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnReaderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnReaderServerApplication.class, args);
+    }
+
+}
